c974baa2d6a4c8aee7fba78bf0860bf227983851
vlsmcin <viniciuslimamelo7@gmail.com>
2025-05-21 18:59:23 -0300

    letra_e

--- Listas/Lista1/letra_e.hs
+++ Listas/Lista1/letra_e.hs
@@ -3,10 +3,10 @@ main = do
        print $ maquinaSomar (read lista :: [Int])
 
 maquinaSomar :: [Int] -> [Int]
-maquinaSomar lst = somaZero lst 0 0
-    where somaZero :: [Int] -> Int -> Int -> [Int]
-          somaZero [] _ _ = [] 
-          somaZero (a:as) x z | z == 2 = []
-                              | a == 0 && x == 0 = [] ++ somaZero as 0 (z + 1)
-                              | a == 0 = [x] ++ somaZero as 0 (z + 1)
-                              | otherwise = somaZero as (x + a) 0
+maquinaSomar lst = somaZero lst 0 0 True
+    where somaZero :: [Int] -> Int -> Int -> Bool -> [Int]
+          somaZero [] _ _ _ = []
+          somaZero (a:as) x z rst | z == 2 = []
+                                       | a == 0 && rst == True = [] ++ somaZero as 0 (z + 1) True 
+                                       | a == 0 = [x] ++ somaZero as 0 (z + 1) True
+                                       | otherwise = somaZero as (x + a) 0 False
